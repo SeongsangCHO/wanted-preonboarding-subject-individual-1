b65f26d59aa2bbd106724371f784ea92d957c224
Seongsangcho <liasqui31@gmail.com>
2021-08-12 19:55:04 +0900

    공통 컴포넌트 export되도록 분리

---
+++ src/Components/common/index.js
@@ -0,0 +1,9 @@
+import Input from "Components/common/Input";
+import Button from "Components/common/Button";
+import Radio from "Components/common/Radio";
+import Modal from "Components/common/Modal/Modal";
+import AddressModal from "Components/common/Modal/AddressModal";
+import SignupModal from "Components/common/Modal/SignupModal";
+import CreditModal from "Components/common/Modal/CreditModal";
+
+export { Input, Button, Radio, Modal, AddressModal, SignupModal, CreditModal };
