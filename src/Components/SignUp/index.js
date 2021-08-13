60a1b0329354e1f2daf808475c2a92ca2862a5a1
Seongsangcho <liasqui31@gmail.com>
2021-08-13 16:36:30 +0900

    signup

---
+++ src/Components/SignUp/index.js
@@ -0,0 +1,21 @@
+import AuthSelector from "Components/SignUp/Input/AuthSelector";
+import PasswordPolicy from "Components/SignUp/PasswordPolicy";
+import Email from "Components/SignUp/Input/Email";
+import Password from "Components/SignUp/Input/Password";
+import PasswordChecker from "Components/SignUp/Input/PasswordChecker";
+import Name from "Components/SignUp/Input/Name";
+import Address from "Components/SignUp/Input/Address";
+import CreditCard from "Components/SignUp/Input/CreditCard";
+import DateOfBirth from "Components/SignUp/Input/DateOfBirth";
+
+export {
+  AuthSelector,
+  Password,
+  PasswordChecker,
+  PasswordPolicy,
+  Email,
+  Name,
+  Address,
+  CreditCard,
+  DateOfBirth,
+};
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,37 +1,26 @@
 import React, { useState } from "react";
-import styled, { css } from "styled-components";
-import Input from "Components/common/Input";
-import Button from "Components/common/Button";
-import Radio from "Components/common/Radio";
-import Modal from "Components/common/Modal/Modal";
-import AddressModal from "Components/common/Modal/AddressModal";
-import SignupModal from "Components/common/Modal/SignupModal";
-import CreditModal from "Components/common/Modal/CreditModal";
-import {
-  isEmail,
-  isPassword,
-  isName,
-  isDateOfBirth,
-  isCreditNum,
-  isEng,
-  isPwNum,
-  isSpe,
-} from "Utils/validator.js";
+import styled from "styled-components";
 import { hashSync } from "Utils/bcrypt";
+import useModalToggle from "Utils/useModalToggle";
 import { AUTH_LEVEL, USER_STORAGE } from "Utils/constants";
 import { loadLocalStorage, saveLocalStorage, autoIncrementUserId } from "Utils/Storage";
-import { ReactComponent as Mail } from "Assets/svg/mail.svg";
-import { ReactComponent as ClosedEye } from "Assets/svg/eye_closed.svg";
-import { ReactComponent as OpenedEye } from "Assets/svg/eye_opened.svg";
-import { ReactComponent as Person } from "Assets/svg/person.svg";
-import { ReactComponent as Map } from "Assets/svg/map.svg";
-import { ReactComponent as Card } from "Assets/svg/card.svg";
-import { ReactComponent as Calendar } from "Assets/svg/calendar.svg";
-import checkIcon from "Assets/svg/check.svg";
+import { isEmail, isPassword, isName, isDateOfBirth, isCreditNum } from "Utils/validator.js";
+import { Button, Modal, AddressModal, SignupModal, CreditModal } from "Components/common";
+import {
+  AuthSelector,
+  Password,
+  PasswordChecker,
+  PasswordPolicy,
+  Email,
+  Name,
+  Address,
+  CreditCard,
+  DateOfBirth,
+} from "Components/SignUp";
+import { ClosedEye, OpenedEye, Map } from "Components/common/SvgExporter";
 
 const SignUp = () => {
-  const [modalType, setModalType] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
+  const { modalToggle, setModalType, isModalOpen, modalType } = useModalToggle();
   const [emailDuplicateStatus, setEmailDuplicateStatus] = useState(SIGNUP_EMAIL_STATUS.default);
   const [emailDuplicateChecked, setEmailDuplicateChecked] = useState(false);
   const [visiblePassword, setVisiblePassword] = useState(true);
@@ -135,60 +124,8 @@ const SignUp = () => {
   };
 
   const toggleModal = (modal) => {
-    setModalOpen(!modalOpen);
+    modalToggle();
     setModalType(modal);
-  };
-
-  const handleSetAuthority = (authority) => {
-    setErrors(initialState);
-    setFormData({
-      ...formData,
-      authority,
-    });
-  };
-
-  const handleSetAddressValue = (address) => {
-    setErrors(initialState);
-    setFormData({
-      ...formData,
-      address,
-    });
-  };
-
-  const handleSetCardNum = (creditCardNum) => {
-    setErrors(initialState);
-    setFormData({
-      ...formData,
-      creditCardNum,
-    });
-  };
-
-  const handleSignUpChange = (e) => {
-    const { name, value } = e.target;
-    setErrors(initialState);
-
-    if (name === "email") {
-      setEmailDuplicateChecked(false);
-    }
-
-    if (name === "pw") {
-      setPasswordError({
-        ...passwordError,
-        eng: isEng(value) >= 0,
-        pwNum: isPwNum(value) >= 0,
-        spe: isSpe(value) >= 0,
-        digit: value.length >= 8,
-      });
-    }
-
-    if (name === "pwCheck") {
-      setPasswordCheckError(value !== formData.pw);
-    }
-
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
   };
 
   const handleSignupSubmit = (e) => {
@@ -217,157 +154,79 @@ const SignUp = () => {
     }
   };
 
+  const handleVisiblePasswordIcon = () => {
+    return visiblePassword ? (
+      <ClosedEye onClick={() => setVisiblePassword(!visiblePassword)} />
+    ) : (
+      <OpenedEye onClick={() => setVisiblePassword(!visiblePassword)} />
+    );
+  };
   return (
     <Wrapper>
       <Form onSubmit={handleSignupSubmit} passwordError={passwordError}>
         <h4>회원가입</h4>
-
-        <Radio
-          name="authority"
+        <AuthSelector
           value={formData.authority}
-          onChange={handleSetAuthority}
-          data={[
-            { value: AUTH_LEVEL.teacher, label: "선생님" },
-            { value: AUTH_LEVEL.parent, label: "부모님" },
-          ]}
           error={errors.authority}
-          errorMessage="원하시는 계정 유형을 선택해 주세요."
+          setFormData={setFormData}
         />
-
-        <EmailWrapper>
-          <Input
-            name="email"
-            value={formData.email}
-            onChange={handleSignUpChange}
-            placeholder="이메일을 입력하세요"
-            icon={<Mail />}
-            error={errors.email}
-            errorMessage={getEmailStatusMessage(emailDuplicateStatus)}
-            successMessage={emailDuplicateChecked && "사용 가능한 이메일 입니다"}
-            width="75%"
-          />
+        <Email
+          formData={formData.email}
+          setFormData={setFormData}
+          errors={errors.email}
+          errorMessage={getEmailStatusMessage(emailDuplicateStatus)}
+          successMessage={emailDuplicateChecked && "사용 가능한 이메일 입니다"}>
           <Button value="중복확인" width="20%" onClick={handleClickDuplicateCheck} />
-        </EmailWrapper>
-
-        <Input
-          name="pw"
+        </Email>
+        <Password
           value={formData.pw}
-          onChange={handleSignUpChange}
-          placeholder="비밀번호를 입력하세요"
-          type={visiblePassword ? "password" : "text"}
-          icon={
-            visiblePassword ? (
-              <ClosedEye onClick={() => setVisiblePassword(!visiblePassword)} />
-            ) : (
-              <OpenedEye onClick={() => setVisiblePassword(!visiblePassword)} />
-            )
-          }
           error={errors.pw}
-          errorMessage="비밀번호를 다시 입력해 주세요"
-        />
-
-        <PasswordPolicy passwordError={passwordError}>
-          <div>
-            <span className="password-pwNum">숫자</span>
-          </div>
-          <div>
-            <span className="password-spe">특수문자</span>
-          </div>
-          <div>
-            <span className="password-eng">영문</span>
-          </div>
-          <div>
-            <span className="password-digit">8자리 이상</span>
-          </div>
-        </PasswordPolicy>
-
-        <Input
-          name="pwCheck"
-          value={formData.pwCheck}
-          onChange={handleSignUpChange}
-          placeholder="비밀번호를 다시 입력하세요"
+          setPasswordError={setPasswordError}
+          icon={handleVisiblePasswordIcon()}
           type={visiblePassword ? "password" : "text"}
-          icon={
-            visiblePassword ? (
-              <ClosedEye onClick={() => setVisiblePassword(!visiblePassword)} />
-            ) : (
-              <OpenedEye onClick={() => setVisiblePassword(!visiblePassword)} />
-            )
-          }
+          setFormData={setFormData}
+        />
+        <PasswordPolicy passwordError={passwordError} />
+
+        <PasswordChecker
+          formData={formData}
           error={passwordCheckError}
-          errorMessage="비밀번호를 다시 입력해 주세요"
+          setPasswordCheckError={setPasswordCheckError}
+          setFormData={setFormData}
+          type={visiblePassword ? "password" : "text"}
+          icon={handleVisiblePasswordIcon()}
         />
-
-        <Input
-          name="name"
-          value={formData.name}
-          onChange={handleSignUpChange}
-          placeholder="이름을 입력하세요"
-          icon={<Person />}
-          error={errors.name}
-          errorMessage="이름을 다시 입력해 주세요"
+        <Name value={formData.name} setFormData={setFormData} error={errors.name} />
+        <Address
+          openModal={() => toggleModal("address")}
+          formData={formData}
+          icon={<Map />}
+          errors={errors}
+          setFormData={setFormData}
         />
-
-        <AddressWrapper>
-          <div className="address-main" onClick={() => toggleModal("address")}>
-            <Input
-              name="address"
-              value={formData.address}
-              placeholder="주소를 입력하세요"
-              icon={<Map />}
-              error={errors.address}
-              errorMessage="주소를 다시 입력해 주세요"
-            />
-            <span>주소검색</span>
-          </div>
-          {formData.address && (
-            <Input
-              name="detailAddress"
-              value={formData.detailAddress}
-              onChange={handleSignUpChange}
-              placeholder="상세주소를 입력하세요"
-              icon={<Map />}
-              error={errors.detailAddress}
-              errorMessage="상세주소를 다시 입력해 주세요"
-            />
-          )}
-        </AddressWrapper>
-
-        <CreditCardWrapper onClick={() => toggleModal("credit")}>
-          <Input
-            name="creditCardNum"
-            value={formData.creditCardNum}
-            placeholder="신용카드 정보를 입력하세요"
-            icon={<Card />}
-            error={errors.creditCardNum}
-            errorMessage="카드번호를 다시 입력해 주세요"
-          />
-          <span>번호입력</span>
-        </CreditCardWrapper>
-
-        <Input
-          name="dateOfBirth"
+        <CreditCard
+          value={formData.creditCardNum}
+          error={errors.creditCardNum}
+          openModal={() => toggleModal("credit")}
+        />
+        <DateOfBirth
           value={formData.dateOfBirth}
-          onChange={handleSignUpChange}
-          placeholder="생년월일 6자리를 입력하세요"
-          icon={<Calendar />}
           error={errors.dateOfBirth}
-          maxLength={6}
-          errorMessage="생년월일을 다시 입력해 주세요"
+          setFormData={setFormData}
         />
 
         <Button type="submit" value="회원가입" marginTop="10px" />
 
-        <Modal isOpen={modalOpen} toggleModal={toggleModal} modalType={modalType}>
+        <Modal isOpen={isModalOpen} toggleModal={toggleModal} modalType={modalType}>
           <>
             {modalType === "success" && <SignupModal />}
             {modalType === "address" && (
-              <AddressModal toggleModal={toggleModal} onSelected={handleSetAddressValue} />
+              <AddressModal toggleModal={toggleModal} setFormData={setFormData} />
             )}
             {modalType === "credit" && (
               <CreditModal
                 creditCard={formData.creditCardNum}
-                handleSetCardNum={handleSetCardNum}
+                setFormData={setFormData}
                 toggleModal={toggleModal}
               />
             )}
@@ -399,99 +258,6 @@ const Form = styled.form`
   @media (max-width: 768px) {
     width: 100%;
     padding: 40px 0;
-  }
-`;
-
-const EmailWrapper = styled.div`
-  ${({ theme }) => theme.flexSet("space-between")};
-`;
-
-const PasswordPolicy = styled.div`
-  ${({ theme }) => theme.flexSet("space-around")};
-
-  > div {
-    span {
-      color: ${({ theme }) => theme.color.borderline};
-      text-align: center;
-      font-size: 15px;
-    }
-    &::before {
-      display: inline-block;
-      background: url(${checkIcon});
-      content: "";
-      width: 20px;
-      height: 16px;
-    }
-    .password-pwNum {
-      ${(props) =>
-        props.passwordError.pwNum &&
-        css`
-          color: ${({ theme }) => theme.color.green};
-          font-weight: 600;
-        `};
-    }
-    .password-eng {
-      ${(props) =>
-        props.passwordError.eng &&
-        css`
-          color: ${({ theme }) => theme.color.green};
-          font-weight: 600;
-        `};
-    }
-    .password-spe {
-      ${(props) =>
-        props.passwordError.spe &&
-        css`
-          color: ${({ theme }) => theme.color.green};
-          font-weight: 600;
-        `};
-    }
-    .password-digit {
-      ${(props) =>
-        props.passwordError.digit &&
-        css`
-          color: ${({ theme }) => theme.color.green};
-          font-weight: 600;
-        `};
-    }
-  }
-`;
-
-const AddressWrapper = styled.div`
-  position: relative;
-
-  span {
-    position: absolute;
-    top: 12.5px;
-    right: 2px;
-    color: ${({ theme }) => theme.color.green};
-    font-size: 13px;
-    font-weight: 600;
-    padding: 10px 50px 13px 0;
-    cursor: pointer;
-    background-color: white;
-  }
-  svg {
-    z-index: 1;
-  }
-`;
-
-const CreditCardWrapper = styled.div`
-  position: relative;
-
-  span {
-    position: absolute;
-    top: 12.5px;
-    right: 2px;
-    color: ${({ theme }) => theme.color.green};
-    font-size: 13px;
-    font-weight: 600;
-    padding: 10px 50px 13px 0;
-    cursor: pointer;
-    background-color: white;
-  }
-  svg {
-    z-index: 1;
   }
 `;
 
